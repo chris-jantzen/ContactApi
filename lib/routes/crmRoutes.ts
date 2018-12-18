64771544945504c64b3c361a517118ed8b1f97fe
Chris Jantzen <jantzeca@mail.uc.edu>
2018-12-17 21:53:46 -0500

    small change of quotation marks

--- lib/routes/crmRoutes.ts
+++ lib/routes/crmRoutes.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import { Request, Response } from 'express';
 import { ContactController } from '../controllers/crmController';
 
 export class Routes {
